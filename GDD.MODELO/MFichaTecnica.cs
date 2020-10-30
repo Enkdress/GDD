8b7304306fbbc545d71614bef53599679b099b37
enkdress <felizjanuk12@gmail.com>
2020-10-29 23:09:58 -0500

    cambio de la libreria de imprimir pdf al proyecto del modelo

---
+++ GDD.MODELO/MFichaTecnica.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GDD.MODELO
+{
+    class MFichaTecnica
+    {
+        private String _genero;
+        private String _ambientacion;
+        private String _publico;
+        private String _estilo;
+        private String _sonido;
+
+        #region getter y setter
+        public String Genero
+        {
+            get { return this._genero; }
+            set { this._genero = value; }
+        }
+        public String Ambientacion
+        {
+            get { return this._ambientacion; }
+            set { this._ambientacion = value; }
+        }
+        public String Publico
+        {
+            get { return this._publico; }
+            set { this._publico = value; }
+        }
+        public String Estilo
+        {
+            get { return this._estilo; }
+            set { this._estilo = value; }
+        }
+        public String Sonido
+        {
+            get { return this._sonido; }
+            set { this._sonido = value; }
+        }
+        #endregion
+
+        public void GuardarPDF()
+        {
+
+        }
+    }
+}
