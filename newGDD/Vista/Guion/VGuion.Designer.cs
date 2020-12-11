59c128f4833f2ecc77bcddcc16c3e687aa70f4a3
Carlos Ortigoza <carlosandresortigoza@gmail.com>
2020-12-11 10:56:11 -0500

    insertando guion

---
+++ newGDD/Vista/Guion/VGuion.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace newGDD.Vista.Guion
+{
+    partial class VGuion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tlpGuardarGuion = new System.Windows.Forms.ToolStripMenuItem();
+            this.tlpBuscarTexto = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(44, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(728, 585);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlpGuardarGuion,
+            this.tlpBuscarTexto});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tlpGuardarGuion
+            // 
+            this.tlpGuardarGuion.Name = "tlpGuardarGuion";
+            this.tlpGuardarGuion.Size = new System.Drawing.Size(76, 24);
+            this.tlpGuardarGuion.Text = "&Guardar";
+            // 
+            // tlpBuscarTexto
+            // 
+            this.tlpBuscarTexto.Name = "tlpBuscarTexto";
+            this.tlpBuscarTexto.Size = new System.Drawing.Size(66, 24);
+            this.tlpBuscarTexto.Text = "&Buscar";
+            // 
+            // VGuion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 672);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "VGuion";
+            this.Text = "VGuion";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tlpGuardar;
+        private System.Windows.Forms.ToolStripMenuItem tlpBuscar;
+        private System.Windows.Forms.ToolStripMenuItem tlpGuardarGuion;
+        private System.Windows.Forms.ToolStripMenuItem tlpBuscarTexto;
+    }
+}
---
+++ newGDD/Vista/Guion/VGuion.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace newGDD.Vista.Guion
+{
+    public partial class VGuion : Form
+    {
+        public VGuion(string path)
+        {
+            InitializeComponent();
+        }
+
+        private void tlpBuscarTexto_ItemClicked(object sender, ToolStripItemClickedEventArgs e) {
+            MessageBox.Show("Buscar");
+        }
+
+        private void tlpGuardarGuion_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            MessageBox.Show("Guardar Guion");
+        }
+    }
+}
