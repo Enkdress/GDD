f1085301bd7cfee827383ee451f4ddafce5a23eb
Enkdress <40218829+Enkdress@users.noreply.github.com>
2020-12-12 13:56:18 -0500

    Añadir recursos funcinando

--- newGDD/Vista/Recursos/VRecurso.cs
+++ newGDD/Vista/Recursos/VRecurso.cs
@@ -18,7 +18,7 @@ namespace newGDD.Vista.Recursos
         private void btnAbrir_Click(object sender, EventArgs e)
         {
             var response = MessageBox.Show("¿El recurso que deseas abrir tiene copyright?", "Abrir recurso", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-            if (response == DialogResult.Yes)
+            if (response == DialogResult.No)
             {
                 try
                 {
@@ -29,11 +29,11 @@ namespace newGDD.Vista.Recursos
                 {
                     Console.WriteLine("Ya existe la carpeta");
                 }
-            } else if (response == DialogResult.No)
+            } else if (response == DialogResult.Yes)
             {
                 try
                 {
-                    Directory.CreateDirectory(this.ubicacionDocumento + @"\Recursos\SIN_Copyright");
+                    Directory.CreateDirectory(this.ubicacionDocumento + @"\Recursos\CON_Copyright");
                     string FolderDefault = Directory.GetCurrentDirectory() + @"\" + this.ubicacionDocumento.Split("/")[1] + @"\" + this.ubicacionDocumento.Split("/")[2] + @"\Recursos\CON_Copyright\";
                     System.Diagnostics.Process.Start(Environment.GetEnvironmentVariable("WINDIR") + @"\explorer.exe", FolderDefault);
                 }
@@ -46,27 +46,58 @@ namespace newGDD.Vista.Recursos
 
         private void btnAnadir_Click(object sender, EventArgs e)
         {
-            var response = MessageBox.Show("¿El recurso que deseas abrir tiene copyright?", "Abrir recurso", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
-            if (response == DialogResult.Yes)
+            var response = MessageBox.Show("¿El recurso que vas a añadir tiene derechos de autor?", "Añadir recurso", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+            if (response == DialogResult.No)
             {
                 try
                 {
-                    Directory.CreateDirectory(this.ubicacionDocumento + "/Recursos/CON_Copyright");
-                    string FolderDefault = this.ubicacionDocumento + "/Recursos/CON_Copyright";
-                    System.Diagnostics.Process.Start(Environment.GetEnvironmentVariable("WINDIR") + @"\explorer.exe", FolderDefault);
+                    Directory.CreateDirectory(this.ubicacionDocumento + @"\Recursos\SIN_Copyright");
+                    OpenFileDialog saveDialog = new OpenFileDialog();
+                    saveDialog.ShowDialog();
+                    if (saveDialog.FileName != "")
+                    {
+                        System.Diagnostics.Process process = new System.Diagnostics.Process();
+                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.Arguments = "/C copy /b \"" + saveDialog.FileName + "\" \"" + Directory.GetCurrentDirectory() + @"\" + this.ubicacionDocumento.Split("/")[1] + @"\" + this.ubicacionDocumento.Split("/")[2] + @"\Recursos\SIN_Copyright\" + "\"";
+                        process.StartInfo = startInfo;
+                        process.Start();
+                    }
+                    else
+                    {
+                        MessageBox.Show("No seleccionaste ningun archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
                 }
                 catch (Exception)
                 {
                     Console.WriteLine("Ya existe la carpeta");
                 }
             }
-            else if (response == DialogResult.No)
+            else if (response == DialogResult.Yes)
             {
                 try
                 {
-                    Directory.CreateDirectory(this.ubicacionDocumento + "/Recursos/SIN_Copyright");
-                    string FolderDefault = this.ubicacionDocumento + "/Recursos/SIN_Copyright";
-                    System.Diagnostics.Process.Start(Environment.GetEnvironmentVariable("WINDIR") + @"\explorer.exe", FolderDefault);
+                    Directory.CreateDirectory(this.ubicacionDocumento + @"\Recursos\CON_Copyright");
+                    OpenFileDialog saveDialog = new OpenFileDialog();
+                    saveDialog.ShowDialog();
+                    if (saveDialog.FileName != "")
+                    {
+                        System.Diagnostics.Process process = new System.Diagnostics.Process();
+                        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                        startInfo.FileName = "cmd.exe";
+                        startInfo.Arguments = "/C copy /b \"" + saveDialog.FileName + "\" \"" + Directory.GetCurrentDirectory() + @"\" + this.ubicacionDocumento.Split("/")[1] + @"\" + this.ubicacionDocumento.Split("/")[2] + @"\Recursos\CON_Copyright\" + "\"";
+                        MessageBox.Show("/C copy /b \"" + saveDialog.FileName + "\" \"" + Directory.GetCurrentDirectory() + @"\" + this.ubicacionDocumento.Split("/")[1] + @"\" + this.ubicacionDocumento.Split("/")[2] + @"\Recursos\CON_Copyright\" + "\"");
+                        process.StartInfo = startInfo;
+                        process.Start();
+                    } 
+                    else
+                    {
+                        MessageBox.Show("No seleccionaste ningun archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
                 }
                 catch (Exception)
                 {
